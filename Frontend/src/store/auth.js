828af4775940187069738935ab62d825bda35e8d
H-C-21 <harshit.c21@iiits.in>
2023-12-03 19:56:17 +0530

    redux

--- Frontend/src/store/auth.js
+++ Frontend/src/store/auth.js
@@ -3,14 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 let authSlice = createSlice({
     name:'auth',
-    initialState: {isLoggedin: false, user: null},
+    initialState: {isLoggedin: false, user: null, role: null, token: null},
 
     reducers:{
-        login(state){
+        login(state, action){
             state.isLoggedin = true;
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+            state.role = action.payload.role;
         },
         logout(state){
             state.isLoggedin = false;
+            state.user = null;
+            state.role = null;
+            state.token = null;
         }
     }
 })
