754b12e6e44de849730cca8db4c41a13fc61add8
H-C-21 <harshit.c21@iiits.in>
2023-12-03 19:53:01 +0530

    course page +

--- Frontend/src/components/CoursePage/Dropdown.jsx
+++ Frontend/src/components/CoursePage/Dropdown.jsx
@@ -3,17 +3,19 @@ import { Accordion, AccordionDetails, AccordionSummary } from "@mui/material";
 
 function Dropdown({currentSection, setCurrentSection, title, num, total, finished, content}) {
 
-
+    console.log('rerender')
 
     function clickHandler(){
+        console.log(title)
         if(currentSection != title){
             setCurrentSection(title)
+            return ;
         } else
         setCurrentSection("")
     }
 
     return (
-        <Accordion expanded={currentSection == title ? true : false} onChange={clickHandler()}>
+        <Accordion expanded={currentSection == title ? true : false} onChange={clickHandler}>
         <AccordionSummary sx={{borderBottom: '1px solid rgba(0, 0, 0, .125)', maxHeight: '64px', minHeight: '64px', '&:hover': {backgroundColor: 'rgba(0, 0, 0, .125)'}}}
             expandIcon={<svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"/></svg>}
             aria-controls="panel1bh-content"
@@ -22,16 +24,22 @@ function Dropdown({currentSection, setCurrentSection, title, num, total, finishe
             <div className="flex">
                 <div></div>
                     <div className="flex flex-col">
-                        <div className="font-weight-bold">{`Section ${num} : ${title}`}</div>
+                        <div className="font-weight-bold">{`Section ${num}: ${title}`}</div>
                         {`${finished} / ${total}`} | {`${finished/total * 100}%`}
                     </div>
                 </div>
         </AccordionSummary>
-
-        <AccordionDetails>
-            
-            {"1. Introduction"}
-        </AccordionDetails>
+        
+        {content.map((lesson, index)=>{
+            return (
+                <AccordionDetails key={index}>
+                    <div className="flex flex-col">
+                        <div className="font-weight-bold">{`Lesson ${index+1}: ${lesson.name}`}</div>
+                    </div>
+                </AccordionDetails>
+            )
+        })
+    }
     </Accordion>
     )
 }
