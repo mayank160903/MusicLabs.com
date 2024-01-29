20f7ee143391c9cb42ed230c480de066a6f9a731
Mohd Rizwan <mohdrizu8170@gmail.com>
2024-01-29 14:48:08 +0530

    admin

--- Frontend/src/dashboard/AdminQuery.jsx
+++ Frontend/src/dashboard/AdminQuery.jsx
@@ -1,36 +1,33 @@
 import { useState } from 'react'
-import './App.css'
+
+import './Sidebar.css';
 import Header from './Header'
 import Sidebar from './Sidebar'
 import Home from './Home'
 import { Routes, Route } from 'react-router-dom';
-
 import Query from './Query';
 
-const AdminQuery = () =>{
+
+
+const AdminAcceptance = () =>{
     const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
 
     const OpenSidebar = () => {
       setOpenSidebarToggle(!openSidebarToggle)
     }
-
+// "overflow-y-auto h-screen w-1000"
     return (
-        <div className=' flex justify-between '>
-         
-          <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
-          
-    <div className="flex flex-col h-screen float-right">
-  <div className="flex-1 overflow-y-auto w-full">
-    <Query />
-  </div>
-</div>
-          
-          
-
+        <div className='grid-container'>
+        
+          <Sidebar  className="sidebar" openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+          <div  className="overflow-y-auto h-screen w-1000"> 
+          <Query />
+            </div>
           </div>
+     
 
       )
 }
 
 
-export default AdminQuery;
+export default AdminAcceptance;
