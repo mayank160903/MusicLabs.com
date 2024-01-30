162d542ec3ec0381a031b9c8718f10ef70fff8b7
Mohd Rizwan <mohdrizu8170@gmail.com>
2024-01-30 14:19:32 +0530

    List of teachers done

--- Backend/routes/AdminRoutes.js
+++ Backend/routes/AdminRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const {getAllQuery} = require('../controllers/AdminController');
+const {getAllQuery , getAllTeachers} = require('../controllers/AdminController');
 
 router.get('/query' , getAllQuery);
+
+router.get('/allteachers' ,getAllTeachers );
 
 
 module.exports = router;
