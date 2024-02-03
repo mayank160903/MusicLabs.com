f8ffd86ba3a4ca95d8f1bf0b71b260023e1aec67
H-C-21 <harshit.c21@iiits.in>
2024-02-04 00:39:02 +0530

    video display also working on teacher side, need to fix section add bugs now

--- Frontend/src/components/CoursePage/MainContent.jsx
+++ Frontend/src/components/CoursePage/MainContent.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { Typography } from "@mui/material";
-import { Fragment, useEffect, useState } from "react";
+import { Fragment, useEffect, useRef, useState } from "react";
 import { useParams } from "react-router";
 // import trialvideo from "../"
 // import video from './update_profile.js - Online Bank System - Visual Studio Code 2023-03-23 00-06-51'
@@ -9,14 +9,20 @@ import { useParams } from "react-router";
 
 function MainContent({currentVideo, currentSection, content, vid}){
 
-    const params = useParams();
-    console.log(params.section)
-
-   
-    useEffect(()=>{
-        console.log(params)
-    },[params.section])
-    
+    const videoRef = useRef(null);
+    const [thumbnailUrl, setThumbnailUrl] = useState('');
+  
+    const generateThumbnail = () => {
+      const video = videoRef.current;
+  
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
+  
+      const thumbnailDataUrl = canvas.toDataURL('image/jpeg');
+      setThumbnailUrl(thumbnailDataUrl);
+    };   
 
 return(
     
@@ -27,10 +33,11 @@ return(
             
         </div>
         <div className="flex justify-center align-top mt-4">
-                <video controls={true}
+                <video controls={true} ref={videoRef} onLoadedData={generateThumbnail}
+                    src = {currentVideo?.url}
                     style={{ borderColor: "black"}}
                     className=" border border-[#B7B7B7] w-[90%] mx-auto cursor-pointer">
-                    <source src={currentVideo?.url} type="video/mp4" />
+                    {thumbnailUrl && <img src={thumbnailUrl} alt="Thumbnail" />}
                 </video>
         </div>
      </div>
